d957d90f6a262c6460d07f7bdb733c7d916da139
erdoganeren <t.eren.erdogan@hotmail.com>
2017-05-10 23:35:47 +0200

    Seiten wurden eingefuegt

---
+++ WalkThisWay/app/src/main/java/at/ac/univie/cosy/hci_gruppe15/walkthisway/Navigation.java
@@ -0,0 +1,13 @@
+package at.ac.univie.cosy.hci_gruppe15.walkthisway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Navigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
